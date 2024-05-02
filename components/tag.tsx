dd983154fa8919ec46d4a7bbc15136dceb71eba4
nhanluongoe <nhanlt18@gmail.com>
2024-05-02 20:32:02 +0700

    chore: update style of tags

--- components/tag.tsx
+++ components/tag.tsx
@@ -8,9 +8,9 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="mr-3 inline rounded-md border border-accent-700 px-1 text-xs text-accent-700 hover:border-accent-500 hover:text-accent-500 dark:border-accent-400 dark:text-accent-400 dark:hover:border-accent-300 dark:hover:text-accent-300"
+      className="inline rounded-md text-sm text-accent-700 hover:text-accent-500 dark:text-accent-400  dark:hover:text-accent-300"
     >
-      {text.split(' ').join('-')}
+      #{text.split(' ').join('-')}
     </Link>
   )
 }
