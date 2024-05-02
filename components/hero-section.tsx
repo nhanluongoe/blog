95e8edd6b6e80b5ae7a44df8859b0069d24dd85f
nhanluongoe <nhanlt18@gmail.com>
2024-05-02 09:15:11 +0700

    chore: update color of hero title

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function HeroSection() {
   return (
     <section className="my-10 flex flex-col items-center justify-center text-center">
-      <h1 className="bg-gradient-to-r from-[#AB271E] to-[#2B5F8C] bg-clip-text text-7xl font-bold leading-normal text-transparent dark:from-[#E84B3C] dark:to-[#5cb5f0]">
+      <h1 className="bg-gradient-to-r from-[#2B5F8C] to-[#AB271E] bg-clip-text text-7xl font-bold leading-normal text-transparent dark:from-[#5cb5f0] dark:to-[#E84B3C]">
         Blogs.
       </h1>
-      <h1 className="bg-gradient-to-r from-[#AB271E] to-[#2B5F8C] bg-clip-text text-7xl font-bold leading-normal text-transparent dark:from-[#E84B3C] dark:to-[#5cb5f0]">
+      <h1 className="bg-gradient-to-r from-[#2B5F8C] to-[#AB271E] bg-clip-text text-7xl font-bold leading-normal text-transparent dark:from-[#5cb5f0] dark:to-[#E84B3C]">
         Open Source.
       </h1>
     </section>
--- components/post-card.tsx
+++ components/post-card.tsx
@@ -16,12 +16,15 @@ export default function PostCard(props: PostProps) {
   return (
     <Link
       href={`/blog/${slug}`}
-      className="group flex w-full max-w-[544px] cursor-pointer flex-col rounded-md border border-gray-200 border-opacity-60 bg-gray-50 p-4 dark:border-gray-900 dark:bg-gray-950"
+      className="group relative flex w-full max-w-[544px] cursor-pointer flex-col rounded-md border border-gray-200 border-opacity-60 bg-gray-50 p-4 dark:border-gray-900 dark:bg-gray-950"
     >
       <h2 className="mt-2 text-2xl font-bold text-gray-900 group-hover:text-accent-500 dark:text-gray-50">
         {title}
       </h2>
-      <time className="text-sm text-accent-700 dark:text-accent-400" dateTime={date}>
+      <time
+        className="absolute right-2 top-2 text-sm text-accent-700 dark:text-accent-400"
+        dateTime={date}
+      >
         {dateToMonthYear(date)}
       </time>
       <p className="my-2 block flex-grow text-gray-600 dark:text-gray-300">{summary}</p>
