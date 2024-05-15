280d6d4e6aa872f678a9532a90e9fceae6fadd2f
nhanluongoe <nhanlt18@gmail.com>
2024-05-15 11:07:59 +0700

    chore: update post section

--- components/post-section.tsx
+++ components/post-section.tsx
@@ -13,9 +13,22 @@ export default function PostSection(props: PostSectionProps) {
 
   return (
     <div className="mt-12">
-      <h1 className="md:leading-14 pb-3 pl-2 text-xl font-bold  tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:text-3xl">
-        Latest Posts
-      </h1>
+      <div className="flex items-center justify-between">
+        <h1 className="md:leading-14 inline-block pb-3 pl-2 text-xl  font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:text-3xl">
+          Latest Posts
+        </h1>
+        {posts.length > MAX_DISPLAY && (
+          <div className="flex justify-end text-base font-medium leading-6">
+            <Link
+              href="/blog"
+              className="text-accent-500 hover:text-accent-600 dark:hover:text-accent-400"
+              aria-label="All posts"
+            >
+              All Posts &rarr;
+            </Link>
+          </div>
+        )}
+      </div>
 
       <div className="grid grid-cols-1 gap-2 md:gap-3">
         {!posts.length && 'No posts found.'}
@@ -23,17 +36,6 @@ export default function PostSection(props: PostSectionProps) {
           return <PostCard key={post.slug} post={post} />
         })}
       </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-accent-500 hover:text-accent-600 dark:hover:text-accent-400"
-            aria-label="All posts"
-          >
-            All Posts &rarr;
-          </Link>
-        </div>
-      )}
     </div>
   )
 }
