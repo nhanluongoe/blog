aa629ee08dc13d15da4dc21a2b81e3ac92480989
nhanluongoe <nhanlt18@gmail.com>
2024-05-21 14:33:55 +0700

    chore: update post layout

--- layouts/post-layout.tsx
+++ layouts/post-layout.tsx
@@ -51,55 +51,28 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
                 <div>
                   <dt className="sr-only">Published on</dt>
                   <dd className="font-small text-base leading-6 text-gray-900 dark:text-gray-900">
-                    <time dateTime={date} className="text-gray-900 dark:text-gray-500">
+                    <time
+                      dateTime={date}
+                      className="md:text-md text-sm text-gray-900 dark:text-gray-500"
+                    >
                       {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
                     </time>
                   </dd>
                 </div>
               </dl>
             </div>
-          </header>
-          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+            <div>
+              {tags && (
+                <div className="my-2 flex flex-wrap justify-center">
+                  {tags.map((tag) => (
+                    <Tag key={tag} text={tag} />
+                  ))}
+                </div>
+              )}
             </div>
-            <footer>
-              <div className="text-sm font-medium leading-5 xl:col-start-1 xl:row-start-2">
-                {tags && (
-                  <div className="py-4 xl:py-8">
-                    <div className="my-2 flex flex-wrap">
-                      {tags.map((tag) => (
-                        <Tag key={tag} text={tag} />
-                      ))}
-                    </div>
-                  </div>
-                )}
-                {(next || prev) && (
-                  <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
-                    {prev && prev.path && (
-                      <div>
-                        <h2 className="text-xs uppercase tracking-wide text-gray-900 dark:text-gray-100">
-                          Previous Article
-                        </h2>
-                        <div className="text-accent-500 hover:text-accent-600 dark:hover:text-accent-400">
-                          <Link href={`/${prev.path}`}>{prev.title}</Link>
-                        </div>
-                      </div>
-                    )}
-                    {next && next.path && (
-                      <div>
-                        <h2 className="text-xs uppercase tracking-wide text-gray-900 dark:text-gray-100">
-                          Next Article
-                        </h2>
-                        <div className="text-accent-500 hover:text-accent-600 dark:hover:text-accent-400">
-                          <Link href={`/${next.path}`}>{next.title}</Link>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                )}
-              </div>
-            </footer>
+          </header>
+          <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+            <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
           </div>
         </div>
       </article>
