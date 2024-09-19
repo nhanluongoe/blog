614ae8c7496e06be779e047887b64d929d73c5af
nhanluongoe <nhanlt18@gmail.com>
2024-09-19 14:28:15 +0700

    chore: add project react-clok

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -22,6 +22,14 @@ const projectsData = [
       'Scalable folder structure boilerplate and starter-kit for Nextjs 14 with App Router support. Using TypeScript, TailwindCSS, NextAuth, Eslint, Prettier, Husky, Commitlint and Lint Staged',
     year: 2024,
   },
+  {
+    title: 'react-clok',
+    repo: 'react-clok',
+    description:
+      'A simple and highly customizable analog clock and digital clock for React applications.',
+    href: 'https://react-clok.vercel.app',
+    year: 2024,
+  },
 ]
 
 export default projectsData
